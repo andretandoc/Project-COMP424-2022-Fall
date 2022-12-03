6e9742ae2c81445db0f76047a95ce50f59c48018
unknown <andrevonvince@gmail.com>
2022-12-03 02:26:48 -0500

    added comments

--- agents/random_agent.py
+++ agents/random_agent.py
@@ -17,25 +17,29 @@ class RandomAgent(Agent):
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
         # Moves (Up, Right, Down, Left)
+        # defining our possible moves
         ori_pos = deepcopy(my_pos)
-        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
-        steps = np.random.randint(0, max_step + 1)
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1)) # possible moves: up down, left right, stay in place
+        steps = np.random.randint(0, max_step + 1) # within max steps, samples with numpy random for steps to take
 
         # Random Walk
+        # general strategy, in this case it just moves randomly
+        # loop does 1 step at a time until steps taken is reached, and adjusts for when a barrier/adversary is in the way
         for _ in range(steps):
             r, c = my_pos
-            dir = np.random.randint(0, 4)
-            m_r, m_c = moves[dir]
-            my_pos = (r + m_r, c + m_c)
+            dir = np.random.randint(0, 4) # chooses a random direction
+            m_r, m_c = moves[dir] # 0 = up, 1 = right, 2 = down, 3 = left
+            my_pos =  (r + m_r, c + m_c) # update our player position by 1, goes in one of the 4 directions only once
+            # current x + move x(-1, 0, 1) and current y + move y(0, 1, -1)
 
             # Special Case enclosed by Adversary
             k = 0
-            while chess_board[r, c, dir] or my_pos == adv_pos:
+            while chess_board[r, c, dir] or my_pos == adv_pos: # while a wall or adversary blocks this move
                 k += 1
                 if k > 300:
                     break
-                dir = np.random.randint(0, 4)
-                m_r, m_c = moves[dir]
+                dir = np.random.randint(0, 4) # it just moves in a random direction again
+                m_r, m_c = moves[dir] 
                 my_pos = (r + m_r, c + m_c)
 
             if k > 300:
@@ -43,9 +47,10 @@ class RandomAgent(Agent):
                 break
 
         # Put Barrier
-        dir = np.random.randint(0, 4)
+        # once the loop is finished, choose a direction to place the barrier in
+        dir = np.random.randint(0, 4) # put it down in a random direction
         r, c = my_pos
-        while chess_board[r, c, dir]:
-            dir = np.random.randint(0, 4)
+        while chess_board[r, c, dir]: # if there's already a barrier/wall in the way
+            dir = np.random.randint(0, 4) # keep trying in random directions around you until you can put one down
 
-        return my_pos, dir
+        return my_pos, dir # return player position(x,y) and direction facing(up, right, down, left)
--- agents/student_agent.py
+++ agents/student_agent.py
@@ -2,6 +2,8 @@
 from agents.agent import Agent
 from store import register_agent
 import sys
+from copy import deepcopy
+from random import randint
 
 
 @register_agent("student_agent")
@@ -26,8 +28,8 @@ class StudentAgent(Agent):
         Implement the step function of your agent here.
         You can use the following variables to access the chess board:
         - chess_board: a numpy array of shape (x_max, y_max, 4)
-        - my_pos: a tuple of (x, y)
-        - adv_pos: a tuple of (x, y)
+        - my_pos: a tuple of (x, y) of where your player is at that moment
+        - adv_pos: a tuple of (x, y) of where the other player is at that moment
         - max_step: an integer
 
         You should return a tuple of ((x, y), dir),
@@ -36,5 +38,54 @@ class StudentAgent(Agent):
 
         Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
         """
+        # Defining possible moves
+        ori_pos = deepcopy(my_pos)
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1)) # possible moves: up, right, down, left
+        steps = randint(0, max_step + 1) # within max steps, samples with random for number of steps to take
+
+        # Moving
+        # General strategy:
+        # Do something completely random, if you can reach the adversary, move there
+        for _ in range(steps):
+
+            r, c = my_pos
+
+            # if adversary is in range, move there
+            if adv_pos in range(my_pos):
+                dir = randint(0,4)
+                # m_r, m_c = 
+                # my_pos =
+            else:
+                dir = randint(0,4) # chooses a random direction
+                m_r, m_c = moves[dir] # 0 = up, 1 = right, 2 = down, 3 = left
+                my_pos =  (r + m_r, c + m_c) # update our player position by 1, goes in one of the 4 directions only once
+                # current x + move x(-1, 0, 1) and current y + move y(0, 1, -1). this is what you're returning.
+
+            k = 0
+            while chess_board[r, c, dir] or my_pos == adv_pos: # while a wall or adversary blocks this move
+                k += 1
+                if k > 300:
+                    break
+            
+                r, c = my_pos
+                dir = randint(0,4)
+                m_r, m_c = moves[dir]
+                my_pos = (r + m_r, c + m_c) # this is what you're updating and returning. (where you end up)
+
+            if k > 300:
+                my_pos = ori_pos
+                break
+
+        # Putting Barrier
+        # once we are at our destination, choose a direction to place the barrier in
+        # if the adversary is next to you, place it in that direction
+        dir = randint(0,4) # put it anywhere
+        r, c = my_pos # mapping my_pos(0) and my_pos(1) to r and c so that we can perform a check
+        
+        while chess_board[r, c, dir]: # if there's already a barrier/wall there,
+            dir = randint(0,4) # keep trying to put it down anywhere randomly
+
+        return my_pos, dir
+
         # dummy return
-        return my_pos, self.dir_map["u"]
+        #return my_pos, self.dir_map["u"]
